cba3641bb492b49256d5a03a0a8359fb7ba5f364
Mateusz Szygenda <mateusz.szygenda@gmail.com>
2011-10-12 17:08:29 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsFortune::Application.config.secret_token = '4b014569506091abe81903d85636957c46760c226045fd907fb129627c67327e2c09e4a67e354d12caba1b0a05d2244c726f575f5e66120c8f18ba9e8724dfbf'
