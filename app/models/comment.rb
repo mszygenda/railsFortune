097578f32411470827a9502761ad952802815ee9
Mateusz Szygenda <mateusz.szygenda@gmail.com>
2011-10-17 13:25:23 +0200

    comments improvements

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :fortune
+  validates :content, :presence => true, :length => 3...250
+  validates :author, :presence => true, :length => 3...50
 end
